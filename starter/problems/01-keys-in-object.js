1b16c0d153fd4289fe7136770bb0d6f45267ebbd
flos06 <floriset@gmail.com>
2022-06-29 10:59:22 +0200

    fixed problem 1

--- starter/problems/01-keys-in-object.js
+++ starter/problems/01-keys-in-object.js
@@ -10,11 +10,23 @@ let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
 keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
+  for (let currentK in animals) {
+    return curentK
+  }
 ***********************************************************************/
 
+
+let array2=[]
+let val = ''
 function keysInObject(obj) {
+  let array = []
   // your code here
+  for (let currentK in obj){
+    array.push(currentK)
+  }
+return array
 }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
