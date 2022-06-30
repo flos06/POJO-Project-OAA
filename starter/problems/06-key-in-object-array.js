663361231aa2887ad19bbfc41a5d701412ed8f13
flos06 <floriset@gmail.com>
2022-06-30 13:45:39 +0200

    06 complete

--- starter/problems/06-key-in-object-array.js
+++ starter/problems/06-key-in-object-array.js
@@ -21,7 +21,23 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // your code here
+
+
+for (let i = 0; i < objArray.length; i++){
+  if (keyString in objArray[i]){
+    return true
+  }
+  
 }
+return false
+
+}
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+keyInObjectArray(objArray, 'planet'); // => true
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
