5b059bc386bd8d0f3e0602af0fbb93c2c909e0cd
flos06 <floriset@gmail.com>
2022-07-01 11:03:14 +0200

    fix 13

--- starter/problems/13-string-converter.js
+++ starter/problems/13-string-converter.js
@@ -13,6 +13,18 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 
 function stringConverter(string) {
   // your code here
+  let stringObj = {}
+
+  for (let i = 0; i < string.length; i++){
+    let currentL = string[i]
+    if (stringObj[currentL] === undefined){
+      stringObj[currentL] = 1
+    }
+    else {
+      stringObj[currentL] += 1
+    }
+  }
+return stringObj
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
