56a0b844a567e6f2491c7d55e75d76b0271e0586
flos06 <floriset@gmail.com>
2022-07-01 11:13:30 +0200

    fixed all problems

--- starter/problems/14-count-scores.js
+++ starter/problems/14-count-scores.js
@@ -28,8 +28,30 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
 function countScores(people) {
-  // your code here
-}
+  let scoresObj = {};
 
+  for (let i = 0; i < people.length; i++) {
+    let personObj = people[i];
+    let name = personObj.name;
+    let score = personObj.score;
+
+    if (scoresObj[name]) {
+      scoresObj[name] += score;
+    } else {
+      scoresObj[name] = score;
+    }
+  }
+  return scoresObj;
+}
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = countScores;
